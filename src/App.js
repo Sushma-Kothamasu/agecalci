2ec293fdd42c83b4218669f9a4342a08e13cb936
Sushma Kothamasu <sushmakothamasu4@gmail.com>
2023-10-29 17:58:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import AgeCalculatorForm from './Agefo';
+import Ageres from './Ageres';
+import {
+  differenceInYears,
+} from 'date-fns';
 
 function App() {
+  const [age, setAge] = useState(null);
+
+  const calculateAge = (birthDate) => {
+    const today = new Date();
+    const birthDateObj = new Date(birthDate);
+    const ageYears = differenceInYears(today, birthDateObj);
+
+    setAge({
+      years: ageYears,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='App'>
+      <div className='old'>
+        <h1 className='mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
+          Age Calculator
+        </h1>
+        <AgeCalculatorForm calculateAge={calculateAge} />
+        {age && <Ageres age={age} />}
+      </div>
+    </section>
   );
 }
 
